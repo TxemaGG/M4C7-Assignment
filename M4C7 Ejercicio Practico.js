177e023538f815b7d11447256de54dbc6617f3d4
TxemaGG <jmggalan@gmail.com>
2025-04-06 22:20:16 +0200

    Initial Commit

---
+++ M4C7 Ejercicio Practico.js
@@ -0,0 +1,20 @@
+/*
+Cree una función JS que acepte 4 argumentos.
+Suma los dos primeros argumentos, luego los dos segundos y multiplícalos.
+Si el número creado es mayor que 50, la consola registra "¡El número es mayor que 50!".
+Si es más pequeño, la consola registra "¡El número es menor que 50!
+Hacerlo con un operador ternario!!??
+*/
+
+function calcularYComparar() {
+    let suma1 = 5 + 3;  
+    let suma2 = 4 + 2;  
+    
+    let resultado = suma1 * suma2; 
+    
+    if (resultado > 50) {
+      console.log("¡El número es mayor que 50!");
+    } else {
+      console.log("¡El número es menor que 50!");
+    }
+  }

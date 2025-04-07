76f59880bc81cd6e79060c4bfcbe671acce51cef
TxemaGG <jmggalan@gmail.com>
2025-04-07 12:03:35 +0200

    Update M4C7 Ejercicio Practico.js

--- M4C7 Ejercicio Practico.js
+++ M4C7 Ejercicio Practico.js
@@ -5,15 +5,17 @@ Si el número creado es mayor que 50, la consola registra "¡El número es mayor
 Si es más pequeño, la consola registra "¡El número es menor que 50!
 */
 
-function calcularYvalorar() {
-    let suma1 = 8 + 10;  
-    let suma2 = 9 + 9;  
+function calcularYvalorar(a, b, c, d) {
+    let suma1 = a + b;  
+    let suma2 = c + d;  
     
     let result = suma1 * suma2; 
     
     if (result > 50) {
-      console.log("¡El número es mayor que 50!");
+        console.log("¡El número es mayor que 50!");
     } else {
-      console.log("¡El número es menor que 50!");
+        console.log("¡El número es menor que 50!");
     }
-  }
+}
+
+calcularYvalorar(8, 10, 9, 9);
